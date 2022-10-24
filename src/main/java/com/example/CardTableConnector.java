1705f829192d1d207cbc4a4ef9a8f64e32182b89
Tor <victoria.schilke@gmail.com>
2022-10-24 08:02:10 -0400

    Add files via upload

---
+++ src/main/java/com/example/CardTableConnector.java
@@ -0,0 +1,16 @@
+package Database_Test;
+
+import java.sql.Connection;
+
+public class CardTableConnector
+{
+    private Connection conn;
+
+    public CardTableConnector(Connection conn)
+    {
+        this.conn = conn;
+    }
+
+
+
+}
