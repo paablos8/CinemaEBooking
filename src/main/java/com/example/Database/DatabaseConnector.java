5136123d4c4b36f0b3193930e16f68ec575d33fb
Tor <victoria.schilke@gmail.com>
2022-10-26 14:55:16 -0400

    Add files via upload

--- src/main/java/com/example/Database/DatabaseConnector.java
+++ src/main/java/com/example/Database/DatabaseConnector.java
@@ -20,7 +20,7 @@ public class DatabaseConnector
      */
     public DatabaseConnector()
     {
-        String serverName = "2.tcp.ngrok.io:17044";
+        String serverName = "0.tcp.ngrok.io:13807";
         String loginCredentials = ";user=sa;password=Team_C2;";
         String settings = "encrypt=true;trustServerCertificate=true";
         String dbURL = "jdbc:sqlserver://"+serverName+loginCredentials+settings;
