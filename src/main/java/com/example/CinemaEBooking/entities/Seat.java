1dfa974270e0dcf9a614a0bce03520f1c4ae127b
JacobiColeman <112585897+JacobiColeman@users.noreply.github.com>
2022-11-04 16:53:58 -0400

    + multiple fields based on seat number(incomplete)

--- src/main/java/com/example/CinemaEBooking/entities/Seat.java
+++ src/main/java/com/example/CinemaEBooking/entities/Seat.java
@@ -15,4 +15,134 @@ public class Seat {
     public void setSeatNumber(int seatNumber) {
         this.seatNumber = seatNumber;
     }
+
+if (seatNumber == "1")
+{
+this.getField("Text1").display = display.visible;
+this.getField("Text2").display = display.hidden;
+this.getField("Text3").display = display.hidden;
+this.getField("Text4").display = display.hidden;
+this.getField("Text5").display = display.hidden;
+this.getField("Text6").display = display.hidden;
+this.getField("Text7").display = display.hidden;
+this.getField("Text8").display = display.hidden;
+this.getField("Text9").display = display.hidden;
+this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "2")
+{
+    this.getField("Text1").display = display.visible;
+this.getField("Text2").display = display.visible;
+this.getField("Text3").display = display.hidden;
+this.getField("Text4").display = display.hidden;
+this.getField("Text5").display = display.hidden;
+this.getField("Text6").display = display.hidden;
+this.getField("Text7").display = display.hidden;
+this.getField("Text8").display = display.hidden;
+this.getField("Text9").display = display.hidden;
+this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "3")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.hidden;
+    this.getField("Text5").display = display.hidden;
+    this.getField("Text6").display = display.hidden;
+    this.getField("Text7").display = display.hidden;
+    this.getField("Text8").display = display.hidden;
+    this.getField("Text9").display = display.hidden;
+    this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "4")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.hidden;
+    this.getField("Text6").display = display.hidden;
+    this.getField("Text7").display = display.hidden;
+    this.getField("Text8").display = display.hidden;
+    this.getField("Text9").display = display.hidden;
+    this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "5")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.visible;
+    this.getField("Text6").display = display.hidden;
+    this.getField("Text7").display = display.hidden;
+    this.getField("Text8").display = display.hidden;
+    this.getField("Text9").display = display.hidden;
+    this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "6")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.visible;
+    this.getField("Text6").display = display.visible;
+    this.getField("Text7").display = display.hidden;
+    this.getField("Text8").display = display.hidden;
+    this.getField("Text9").display = display.hidden;
+    this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "7")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.visible;
+    this.getField("Text6").display = display.visible;
+    this.getField("Text7").display = display.visible;
+    this.getField("Text8").display = display.hidden;
+    this.getField("Text9").display = display.hidden;
+    this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "8")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.visible;
+    this.getField("Text6").display = display.visible;
+    this.getField("Text7").display = display.visible;
+    this.getField("Text8").display = display.visible;
+    this.getField("Text9").display = display.hidden;
+    this.getField("Text10").display = display.hidden;;
+}
+else if (seatNumber == "9")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.visible;
+    this.getField("Text6").display = display.visible;
+    this.getField("Text7").display = display.visible;
+    this.getField("Text8").display = display.visible;
+    this.getField("Text9").display = display.visible;
+    this.getField("Text10").display = display.hidden;
+}
+else if (seatNumber == "10")
+{
+    this.getField("Text1").display = display.visible;
+    this.getField("Text2").display = display.visible;
+    this.getField("Text3").display = display.visible;
+    this.getField("Text4").display = display.visible;
+    this.getField("Text5").display = display.visible;
+    this.getField("Text6").display = display.visible;
+    this.getField("Text7").display = display.visible;
+    this.getField("Text8").display = display.visible;
+    this.getField("Text9").display = display.visible;
+    this.getField("Text10").display = display.hidden;
 }
