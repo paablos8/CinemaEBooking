090686462c9e2dcd5c5d2f8d9f40611d0348fbee
Tor <victoria.schilke@gmail.com>
2022-11-05 16:40:46 -0400

    Update DatabaseConnector.java

--- src/main/java/com/example/Database/DatabaseConnector.java
+++ src/main/java/com/example/Database/DatabaseConnector.java
@@ -20,7 +20,7 @@ public class DatabaseConnector
      */
     public DatabaseConnector()
     {
-        String serverName = "4.tcp.ngrok.io:14635";
+        String serverName = "6.tcp.ngrok.io:15508";
         String loginCredentials = ";user=sa;password=Team_C2;";
         String settings = "encrypt=true;trustServerCertificate=true";
         String dbURL = "jdbc:sqlserver://"+serverName+loginCredentials+settings;
