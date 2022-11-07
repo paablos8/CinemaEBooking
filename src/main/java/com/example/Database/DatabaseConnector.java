63e19a959109b245bbb78487f74aac415c2dd974
Tor <victoria.schilke@gmail.com>
2022-11-07 11:54:05 -0500

    Added Movie methods
    can't get upcoming movies yet, as showtimes aren't implemented yet
    can get all movies and movies based on a category
    can't get movies based on keywords yet apart from category

--- src/main/java/com/example/Database/DatabaseConnector.java
+++ src/main/java/com/example/Database/DatabaseConnector.java
@@ -14,13 +14,14 @@ public class DatabaseConnector
     private UserTableConnector utc = null;
     private CardTableConnector ctc = null;
     private AddressTableConnector atc = null;
+    private MovieTableConnector mtc = null;
 
     /**
      * Creates DatabaseConnector object and connects to server.
      */
     public DatabaseConnector()
     {
-        String serverName = "6.tcp.ngrok.io:15508";
+        String serverName = "8.tcp.ngrok.io:18810";
         String loginCredentials = ";user=sa;password=Team_C2;";
         String settings = "encrypt=true;trustServerCertificate=true";
         String dbURL = "jdbc:sqlserver://"+serverName+loginCredentials+settings;
@@ -37,6 +38,7 @@ public class DatabaseConnector
             utc = new UserTableConnector(conn);
             ctc = new CardTableConnector(conn);
             atc = new AddressTableConnector(conn);
+            mtc = new MovieTableConnector(conn);
         }
     }
 
@@ -59,6 +61,10 @@ public class DatabaseConnector
     }
 
     //User Methods
+    public boolean createNewUser(String fName, String lName, long pNum,
+                                 String email, boolean promoOp, boolean isAdmin,
+                                 String password)
+    {return utc.createNewUser(fName,lName,pNum,email,promoOp,isAdmin,password);}
     public int verifyLogin(String email, String password){return utc.verifyLogin(email,password);}
     public String getUserFirstName(int userID){return utc.getUserFirstName(userID);}
     public String getUserLastName(int userID){return utc.getUserLastName(userID);}
@@ -68,10 +74,6 @@ public class DatabaseConnector
     public Status getUserStatus(int userID){return utc.getUserStatus(userID);}
     public String getUserEmail(int userID){return utc.getUserEmail(userID);}
     public String getUserPassword(int userID){return utc.getUserPassword(userID);}
-    public boolean createNewUser(String fName, String lName, long pNum,
-                                 String email, boolean promoOp, boolean isAdmin,
-                                 String password)
-    {return utc.createNewUser(fName,lName,pNum,email,promoOp,isAdmin,password);}
     public User createUserObject(int userID){return utc.createUserObject(userID);}
     public boolean resetPassword(String email,String password){return utc.resetPassword(email,password);}
     public boolean changeFirstName(int userID,String fName){return utc.changeFirstName(userID,fName);}
@@ -107,17 +109,51 @@ public class DatabaseConnector
     {return ctc.changeCardZipCode(cardNum,zipCode);}
 
     //Address Methods
+    public int createNewAddress(int userID,String stAdd,String city,String state,String country,int zip)
+    {return atc.createNewAddress(userID,stAdd,city,state,zip);}
     public String getStreetAddress(int userID){return atc.getStreetAddress(userID);}
     public String getCityCounty(int userID){return atc.getCityCounty(userID);}
     public String getStateRegion(int userID){return atc.getStateRegion(userID);}
     public String getCountry(int userID){return atc.getCountry(userID);}
     public int getZipCode(int userID){return atc.getZipCode(userID);}
-    public int createNewAddress(int userID,String stAdd,String city,String state,String country,int zip)
-    {return atc.createNewAddress(userID,stAdd,city,state,zip);}
     public boolean changeStreetAddress(int userID,String stAdd){return atc.changeUserStreetAddress(userID,stAdd);}
     public boolean changeCityCounty(int userID,String city){return atc.changeUserCityCounty(userID,city);}
     public boolean changeStateRegion(int userID,String state){return atc.changeUserStateRegion(userID,state);}
     public boolean changeCountry(int userID,String country){return atc.changeUserCountry(userID,country);}
     public boolean changeZipCode(int userID,int zip){return atc.changeUserZipCode(userID,zip);}
 
+    //Movie Methods
+    public boolean createNewMovie(String title,String ageRating,int yearRelease,String producer,String director,
+                                  String cast,int IMDBRating,int RTRating,String trailerURL,String posterURL,
+                                  String category, String synopsis,int durationMin)
+    {
+        return mtc.createNewMovie(title,ageRating,yearRelease,producer,director,cast,IMDBRating,RTRating,trailerURL
+        ,posterURL,category,synopsis,durationMin);
+    }
+    String [] getTitlesInCategory(String category){return mtc.getTitlesInCategory(category);}
+    String [] getAllTitles(){return mtc.getAllTitles();}
+    String getAgeRating(String title){return mtc.getAgeRating(title);}
+    int getYearRelease(String title){return mtc.getYearRelease(title);}
+    String getProducer(String title){return mtc.getProducer(title);}
+    String getCast(String title){return mtc.getCast(title);}
+    String getDirector(String title){return mtc.getDirector(title);}
+    int getIMDBRating(String title){return mtc.getIMDBRating(title);}
+    int getRTRating(String title){return mtc.getRTRating(title);}
+    String getTrailerURL(String title){return mtc.getTrailerURL(title);}
+    String getPosterURL(String title){return mtc.getPosterURL(title);}
+    String getCategory(String title){return mtc.getCategory(title);}
+    String getSynopsis(String title){return mtc.getSynopsis(title);}
+    int getDuration(String title){return mtc.getDuration(title);}
+    boolean changeAgeRating(String title,String ageRate){return mtc.changeAgeRating(title,ageRate);}
+    boolean changeYearRelease(String title,int yearRelease){return mtc.changeYearRelease(title,yearRelease);}
+    boolean changeProducer(String title,String producer){return mtc.changeProducer(title,producer);}
+    boolean changeCast(String title,String cast){return mtc.changeCast(title,cast);}
+    boolean changeDirector(String title,String director){return mtc.changeDirector(title,director);}
+    boolean changeIMDBRating(String title,int IMDBRating){return mtc.changeIMDBRating(title,IMDBRating);}
+    boolean changeRTRating(String title,int RTRating){return mtc.changeRTRating(title,RTRating);}
+    boolean changeTrailerURL(String title,String trailerURL){return mtc.changeTrailerURL(title,trailerURL);}
+    boolean changePosterURL(String title,String posterURL){return mtc.changePosterURL(title,posterURL);}
+    boolean changeCategory(String title,String category){return mtc.changeCategory(title,category);}
+    boolean changeSynopsis(String title,String synopsis){return mtc.changeSynopsis(title,synopsis);}
+    boolean changeDuration(String title,int duration){return mtc.changeDuration(title,duration);}
 }
---
+++ src/main/java/com/example/Database/MovieTableConnector.java
@@ -0,0 +1,142 @@
+package com.example.Database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MovieTableConnector extends SQL_GetSet
+{
+    private Connection conn;
+    /**
+     * Creates Movie Table Connector with given connection.
+     * Connection should be linked to server prior to creating this object.
+     * @param conn Valid connection
+     */
+    public MovieTableConnector(Connection conn) {super(conn);this.conn = conn;}
+
+    public boolean createNewMovie(String title,String ageRating,int yearRelease,String producer,String director,String cast,
+                                  int IMDBRating,int RTRating,String trailerURL,String posterURL,String category,
+                                  String synopsis,int durationMin)
+    {
+        if(!(verifyString(title)&&verifyString(producer)&&verifyString(director)&&verifyString(cast)&&
+                verifyString(trailerURL) && verifyString(posterURL) && verifyString(synopsis)))
+        {
+            return false;
+        }
+
+        try(Statement stmt = conn.createStatement())
+        {
+            String SQL = "INSERT INTO Movie Titles VALUES ('"+title+"',"+ageRating+","+yearRelease+",'"+producer+"','"
+                    +cast+"','"+director+","+IMDBRating+","+RTRating+",'"+trailerURL+"','"+category+
+                    "','"+synopsis+"',"+durationMin+",'"+posterURL+"'"+")";
+            stmt.executeUpdate(SQL);
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    // get titles of movies in specific category
+    String [] getTitlesInCategory(String category)
+    {
+        return getMany(category,"Movie Titles","Category","Title");
+    }
+
+    String [] getAllTitles()
+    {
+        return getAll("Movie Titles","Title");
+    }
+
+    // ageRating
+    String getAgeRating(String title){return get(title,"Movie Titles","Title","Age Rating");}
+    boolean changeAgeRating(String title,String ageRate)
+    {
+        if(verifyString(ageRate))return false;
+        return update(title,"Movie Titles","Title","Age Rating",ageRate);
+    }
+
+    // yearRelease
+    int getYearRelease(String title){return get(title,"Movie Titles","Title","Year Released");}
+    boolean changeYearRelease(String title,int yearRelease)
+    {
+        return update(title,"Movie Titles","Title","Year Released",yearRelease);
+    }
+
+    // producer
+    String getProducer(String title){return get(title,"Movie Titles","Title","Producer");}
+    boolean changeProducer(String title,String producer)
+    {
+        if(verifyString(producer))return false;
+        return update(title,"Movie Titles","Title","Producer",producer);
+    }
+
+    // cast
+    String getCast(String title){return get(title,"Movie Titles","Title","Cast");}
+    boolean changeCast(String title,String cast)
+    {
+        if(verifyString(cast))return false;
+        return update(title,"Movie Titles","Title","Cast",cast);
+    }
+
+    // director
+    String getDirector(String title){return get(title,"Movie Titles","Title","Director");}
+    boolean changeDirector(String title,String director)
+    {
+        if(verifyString(director))return false;
+        return update(title,"Movie Titles","Title","Director",director);
+    }
+
+    // IMDBRating
+    int getIMDBRating(String title){return get(title,"Movie Titles","Title","IMDB Rating");}
+    boolean changeIMDBRating(String title,int IMDBRating)
+    {
+        return update(title,"Movie Titles","Title","IMDB Rating",IMDBRating);
+    }
+
+    // RTRating
+    int getRTRating(String title){return get(title,"Movie Titles","Title","Rotten Tomatoes");}
+    boolean changeRTRating(String title,int RTRating)
+    {
+        return update(title,"Movie Titles","Title","Rotten Tomatoes",RTRating);
+    }
+
+    // trailerURL
+    String getTrailerURL(String title){return get(title,"Movie Titles","Title","Trailer URL");}
+    boolean changeTrailerURL(String title,String trailerURL)
+    {
+        if(verifyString(trailerURL))return false;
+        return update(title,"Movie Titles","Title","Trailer URL",trailerURL);
+    }
+
+    // posterURL
+    String getPosterURL(String title){return get(title,"Movie Titles","Title","PosterURL");}
+    boolean changePosterURL(String title,String posterURL)
+    {
+        if(verifyString(posterURL))return false;
+        return update(title,"Movie Titles","Title","PosterURL",posterURL);
+    }
+
+    // categoryID
+    String getCategory(String title){return get(title,"Movie Titles","Title","Category");}
+    boolean changeCategory(String title,String category)
+    {
+        return update(title,"Movie Titles","Title","Category ID",category);
+    }
+
+    // synopsis
+    String getSynopsis(String title){return get(title,"Movie Titles","Title","Synopsis");}
+    boolean changeSynopsis(String title,String synopsis)
+    {
+        return update(title,"Movie Titles","Title","Synopsis",synopsis);
+    }
+
+    // durationMin
+    int getDuration(String title){return get(title,"Movie Titles","Title","Duration");}
+    boolean changeDuration(String title,int duration)
+    {
+        return update(title,"Movie Titles","Title","Duration",duration);
+    }
+}
