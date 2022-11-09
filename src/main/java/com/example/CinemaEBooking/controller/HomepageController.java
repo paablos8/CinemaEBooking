671f961cfee0e80ac162a7ac79ef158526bb6f3f
FatFrederik <dorhasenbroek@gmail.com>
2022-11-09 16:55:41 -0500

    HomePage needs titles

--- src/main/java/com/example/CinemaEBooking/controller/HomepageController.java
+++ src/main/java/com/example/CinemaEBooking/controller/HomepageController.java
@@ -1,19 +1,27 @@
 package com.example.CinemaEBooking.controller;
-
+import org.springframework.ui.ModelMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.example.Database.DatabaseConnector;
+import com.example.CinemaEBooking.entities.Movie;
 
 @Controller
 public class HomepageController {
+
+    private DatabaseConnector db = new DatabaseConnector();
     
     @RequestMapping("/")
-    public String home (){
-
+    public String home(){
         return "redirect:/homePage";
     }
 
     @RequestMapping("/homePage")
-    public String homePage (){
+    public String homePage (ModelMap model){
+        
+        String titles[] = db.getAllTitles();
+        
+        
+    
 
         return "homePage";
     }
