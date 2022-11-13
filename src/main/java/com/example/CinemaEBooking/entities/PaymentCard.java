5668735b34fe976d92b4f0d30ab5aa3a0c3e576b
FatFrederik <dorhasenbroek@gmail.com>
2022-11-12 22:05:30 -0500

    Payment Crad update

--- src/main/java/com/example/CinemaEBooking/entities/PaymentCard.java
+++ src/main/java/com/example/CinemaEBooking/entities/PaymentCard.java
@@ -1,20 +1,62 @@
 package com.example.CinemaEBooking.entities;
 
 import java.util.Date;
+import java.util.Properties;
+
+import com.fasterxml.jackson.databind.annotation.JsonAppend.Prop;
 
 public class PaymentCard {
     private String nameOnCard;
     private int cardNumber;
-    private String billingAddress;
     private int cvv;
-    private Date expirationDate;
-    
+    private String expirationDate;
+    private String street;
+    private String city;
+    private String state;
+    private int zip;
+
+
     //private Booking[] bookings; //not consistent to our UML diagram, but I think an array to store the bookings would be more appropriate
 
 
-    
+
     //getter and setter
+
+
+
     
+        public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
         public String getNameOnCard() {
         return nameOnCard;
     }
@@ -31,19 +73,12 @@ public class PaymentCard {
         this.cardNumber = cardNumber;
     }
 
-    public String getBillingAddress() {
-        return billingAddress;
-    }
 
-    public void setBillingAddress(String billingAddress) {
-        this.billingAddress = billingAddress;
-    }
-
-    public Date getExpirationDate() {
+    public String getExpirationDate() {
         return expirationDate;
     }
 
-    public void setExpirationDate(Date expirationDate) {
+    public void setExpirationDate(String expirationDate) {
         this.expirationDate = expirationDate;
     }
 
@@ -65,5 +100,5 @@ public class PaymentCard {
     }
     */
 
-    
+
 }
