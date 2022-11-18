8a389380f19d9ce990ca75a53bd8c8290218dcfc
Tor <victoria.schilke@gmail.com>
2022-11-18 15:00:18 -0500

    server online 11-18 3:00pm

--- src/main/java/com/example/Database/DatabaseConnector.java
+++ src/main/java/com/example/Database/DatabaseConnector.java
@@ -24,7 +24,7 @@ public class DatabaseConnector
      */
     public DatabaseConnector()
     {
-        String serverName = "8.tcp.ngrok.io:17883";
+        String serverName = "8.tcp.ngrok.io:15140";
         String loginCredentials = ";user=sa;password=Team_C2;";
         String settings = "encrypt=true;trustServerCertificate=true";
         String dbURL = "jdbc:sqlserver://"+serverName+loginCredentials+settings;
