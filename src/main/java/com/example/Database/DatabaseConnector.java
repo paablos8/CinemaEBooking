bc1375d4bfa89b3dbe26817779e538fe53e9af25
Tor <victoria.schilke@gmail.com>
2022-11-22 10:55:13 -0500

    server online 10:54 11-22
    internet connection on my laptop dropped for a second

--- src/main/java/com/example/Database/DatabaseConnector.java
+++ src/main/java/com/example/Database/DatabaseConnector.java
@@ -24,7 +24,7 @@ public class DatabaseConnector
      */
     public DatabaseConnector()
     {
-        String serverName = "0.tcp.ngrok.io:17449";
+        String serverName = "4.tcp.ngrok.io:11931";
         String loginCredentials = ";user=sa;password=Team_C2;";
         String settings = "encrypt=true;trustServerCertificate=true";
         String dbURL = "jdbc:sqlserver://"+serverName+loginCredentials+settings;
