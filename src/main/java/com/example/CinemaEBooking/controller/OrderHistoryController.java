4b1e46ac8436b100b8cb23e68cb6589412fb8b71
FatFrederik <dorhasenbroek@gmail.com>
2022-12-07 18:19:59 -0500

    order history working

--- src/main/java/com/example/CinemaEBooking/controller/OrderHistoryController.java
+++ src/main/java/com/example/CinemaEBooking/controller/OrderHistoryController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.CinemaEBooking.entities.Movie;
+import com.example.CinemaEBooking.entities.User;
 import com.example.Database.DatabaseConnector;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -16,9 +21,36 @@ public class OrderHistoryController {
     DatabaseConnector db = DatabaseConnector.getInstance();
     
     @RequestMapping(value = "/orderHistory", method = RequestMethod.GET)
-    public String showOrderHistory(ModelMap model) {
-        
-        
+    public String showOrderHistory(ModelMap model, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User currentUser = (User) session.getAttribute("currentUser");
+
+        int [] orders = db.getBookings(currentUser.getUserId());
+        for (int i = 0; i < orders.length; i++) {
+            String name = "movie" + i;
+            String child = "child" + i;
+            String adult = "adult" + i;
+            String senior = "senior" + i;
+            String total = "total" + i;
+
+            
+
+            int showID = db.getShowtimeID(orders[i]);
+            String title = db.getShowTitle(showID);
+            String numChild = "Child tickets: " + db.getChildTickets(orders[i]);
+            String numAdult = "Adult tickets: " + db.getAdultTickets(orders[i]);
+            String numSenior = "Senior tickets: " + db.getSeniorTickets(orders[i]);
+            String totalPrice =  "Total: $" + db.getTotal(orders[i]);
+
+           
+            model.addAttribute(name, title);
+            model.addAttribute(child, numChild);
+            model.addAttribute(adult, numAdult);
+            model.addAttribute(senior, numSenior);
+            model.addAttribute(total, totalPrice);
+          }
+
+       
 
 
        
