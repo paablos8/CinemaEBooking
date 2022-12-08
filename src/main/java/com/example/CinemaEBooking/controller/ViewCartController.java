fc34685c700da1fcd50a6538c1cc2a59fa77b0c3
FatFrederik <dorhasenbroek@gmail.com>
2022-12-08 13:38:16 -0500

    view cart added pablos stuff

--- src/main/java/com/example/CinemaEBooking/controller/ViewCartController.java
+++ src/main/java/com/example/CinemaEBooking/controller/ViewCartController.java
@@ -5,10 +5,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.example.CinemaEBooking.entities.Booking;
 import com.example.CinemaEBooking.entities.PaymentCard;
 import com.example.CinemaEBooking.entities.User;
 import com.example.Database.DatabaseConnector;
@@ -24,12 +26,15 @@ public class ViewCartController {
 
     @RequestMapping(value = "/viewcart", method = RequestMethod.GET)
     public String showViewCartPage(Model model, HttpServletRequest request) {
-
+        model.addAttribute("submitCheckout", new Booking());
 
         //Get current logged in user
         HttpSession session = request.getSession();
         User currentUser = (User) session.getAttribute("currentUser");
         int userId = currentUser.getUserId();
+
+        //Get show ID the tickets will be booked for
+        int showId = (int) session.getAttribute("showId");
 
         //Get Card data
         PaymentCard[] storedPaymentCards = db.createCardObjects(userId);
@@ -82,13 +87,63 @@ public class ViewCartController {
             model.addAttribute("card3Zip",zips[2]);
     }
 
+    System.out.println("This is the showId: " + showId);
+
+    return "viewcart";
+}
 
 
+    //Submit the Checkout
+@RequestMapping(value = "/viewcart", method = RequestMethod.POST)
+public Object submitCheckout(@ModelAttribute("submitCheckout") Booking booking, Model model, HttpServletRequest request) {
+    HttpSession session = request.getSession();
+    User currentUser = (User) session.getAttribute("currentUser");
+    int userId = currentUser.getUserId();
 
+    //Get show ID the tickets will be booked for
+    int showId = (Integer) session.getAttribute("showId");
 
-        return "viewcart";
-    }   
+    String promotionCode = booking.getPromoCode();
+    String[] allPromotionCodes = db.getAllPromotionCodes();
+    boolean promoFound = false;
 
+    for (int i = 0; i < allPromotionCodes.length; i++) {
+        if (promotionCode.equals(allPromotionCodes[i])) {
+            promoFound = true;
+        }
+    }
 
-    
+    if (promoFound == true) {
+        double percentOff = db.getPercentOff(promotionCode);
+
+        double sum = 19.00 * (1-percentOff);
+
+        System.out.println("After Promotion and Tickets: " + sum);
+        model.addAttribute("afterPromotion", sum);
+        double promotionSaves = 19.00 * percentOff;
+
+        double totalSum = sum * 1.07;
+        System.out.println("The Total Sum after Tax is: " + totalSum);
+        model.addAttribute("totalSum", totalSum);
+        String message = "Your Promotion has been applied! You get " + percentOff + "% and saved $" + promotionSaves;
+        model.addAttribute("promotionApplied", message);
+        model.addAttribute("totalSum");
+
+        //Create new booking
+        int bookingId = db.createNewBooking(userId, userId, promotionCode, showId, 2, 1, 0);
+        
+        //get the total price
+        double totalPrice = db.getTotal(bookingId);
+        System.out.println(totalPrice);
+        
+        return "orderSummary";
+        }
+
+    else {
+
+        model.addAttribute("promotionResult", "The used Promotion Code is not valid!");
+        return "viewcart";  
+        
+    }
+    }       
 }
